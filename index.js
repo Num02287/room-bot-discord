bd7f7a822572aaac735c5e7f69c125e39b789959
Num02287 <nongnoum4922@gmail.com>
2026-05-18 23:01:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -180,9 +180,9 @@ const embed = new EmbedBuilder()
         return interaction.editReply({ content: "🔓 ปลดล็อกห้องแล้ว" });
       }
       
-     if (interaction.customId === "hide") {
+      if (interaction.customId === "hide") {
         await channel.permissionOverwrites.edit(interaction.guild.id, { ViewChannel: false });
-        if (allowRoleId) await channel.permissionOverwrites.edit(allowRoleId, { ViewChannel: false }).catch(()=>{});
+      if (allowRoleId) await channel.permissionOverwrites.edit(allowRoleId, { ViewChannel: false }).catch(() => {});
         await channel.permissionOverwrites.edit(member.id, { ViewChannel: true, Connect: true });
         return interaction.editReply({ content: "🙈 ซ่อนห้องแล้ว" });
       }
